872ff7a925c1ac08b714de40925418ab77c40164
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-17 12:05:30 +0700

    deps: add husky and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,21 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+const types = [
+    'build',
+	'ci',
+	'docs',
+	'feat',
+	'fix',
+	'perf',
+	'refactor',
+	'revert',
+	'style',
+	'test',
+    'ui',
+    'deps'
+];
+
+module.exports = {
+    extends: ['@commitlint/config-angular'],
+    rules: {
+        'type-enum': [2, 'always', types],
+    }
+};
