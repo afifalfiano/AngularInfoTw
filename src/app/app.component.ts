16db70b6b8182e16be823c892504a91b487e6b01
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-08-17 12:55:27 +0700

    fix: update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'AngularInfoTwz';
   a =    '';
+  asda =    '';
+  azc =    '';
+  acxz =    '';
+  aasd =    '';
 }
